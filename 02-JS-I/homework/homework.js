5d588f6266852cd3e30cb9418973734af6b3b8d1
SantiagoPoeta <poetasantiago81@gmail.com>
2022-06-16 11:45:52 -0300

    Terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -44,7 +44,7 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  var resta = y - x;
+  var resta = x - y;
   return resta;
   
 }
@@ -105,11 +105,10 @@ function mayorQueCincuenta(num) {
   
 }
 
-function obtenerResto(x, y) {
+function obtenerResto(x,y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
- var resto= x % y;
-  
+ return x % y 
 }
 
 function esPar(num) {
@@ -207,10 +206,9 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = ("Hola" + nombre);
-  
+  var saludo ="Hola" +" "+ nombre + "!";
+  return saludo; 
 }
-
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
@@ -229,7 +227,8 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  var area= base*altura;
+  var area= (base * altura )/2;
+  return area;
 
 }
 
@@ -238,8 +237,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var dolares= euro*1.20;
-  
+  var dolares= euro* 1.20;
+  return dolares;
 }
 
 
@@ -249,16 +248,17 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length>1){
-    console.log("Dato incorrecto");
+  if(letra==="a" || letra==="e" ||letra==="i" ||letra==="o" ||letra==="u"){
+   return ("Es vocal");
+  } else {
+    if(letra.length>1){
+      return "Dato incorrecto";
+    }
+    else{
+      return "Dato incorrecto";
+    }
   }
-  switch(letra){
-    case "a" || "e" || "i" || "o" || "u":
-      console.log("Es vocal");
-      break;
-      default:
-        console.log("Dato incorrecto");
-  }
+   
 }
 
 
